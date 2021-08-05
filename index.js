a4af2ea5ba65d4266aac509abff9f9cf84a6a2a0
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-08-05 10:29:30 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('web 44 rocks!!!!!!!!')
+require('dotenv').config() // This allows to "inject" fake environment variables
+
+const { PORT } = require('./config')
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Webb 44 Is Awesome!</h1>
+    `)
+})
+
+server.use('*', (req, res, next) => {
+    res.json({
+        message: 'web 44 is awesome indeed'
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
